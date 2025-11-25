b77b35cd0680e073de584e44ac54ad91ab03ffb2
ishan <ishanbashyal11@gmail.com>
2025-11-25 13:43:28 +0545

    Login, Registration and Start page UI made

--- app/src/main/java/com/example/cleantrack/ui/theme/Color.kt
+++ app/src/main/java/com/example/cleantrack/ui/theme/Color.kt
@@ -3,9 +3,14 @@ package com.example.cleantrack.ui.theme
 import androidx.compose.ui.graphics.Color
 
 val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
+val White = Color(0xFFFFFFFF)
+val Black = Color(0xFF000000)
+val Blue = Color(0xFF5D78BC)
+val PurpleGrey80 = Color(0xFFDAEEDF)
 val Pink80 = Color(0xFFEFB8C8)
 
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+val Green = Color(0xFF63BB5A)
+val Red = Color(0xFFFF0000)
