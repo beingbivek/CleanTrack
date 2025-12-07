0cbae6c49483521e7cc237b5442f3f9a0725bdca
ishan <ishanbashyal11@gmail.com>
2025-12-07 21:20:18 +0545

    Jitpack maven added in settings gradle

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -16,6 +16,8 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
+        // JitPack repository for MapLibre dependencies
+        maven { url = uri("https://jitpack.io") }
     }
 }
 
