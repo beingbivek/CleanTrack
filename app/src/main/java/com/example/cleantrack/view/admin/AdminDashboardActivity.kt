3c7c7805456ebace119c9557f9b9c4514a6e9600
Bivek Thapa <beingbivek@gmail.com>
2025-12-15 20:11:53 +0545

    fixing import of lazy items

--- app/src/main/java/com/example/cleantrack/view/admin/AdminDashboardActivity.kt
+++ app/src/main/java/com/example/cleantrack/view/admin/AdminDashboardActivity.kt
@@ -20,6 +20,7 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Delete
