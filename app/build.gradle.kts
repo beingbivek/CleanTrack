dda6e835332bda9c2e807c2089ac7770bc5be9ba
Bivek Thapa <beingbivek@gmail.com>
2025-12-15 17:01:35 +0545

    again importing FCM

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -71,9 +71,6 @@ dependencies {
     // Import the Firebase BOM
     implementation(platform("com.google.firebase:firebase-bom:33.0.0"))
 
-    implementation(libs.firebase.auth)
-    implementation(libs.firebase.firestore)
-
     // REQUIRED: Google Sign-In Client
     implementation("com.google.android.gms:play-services-auth:21.0.0") // Use the latest stable version
 
