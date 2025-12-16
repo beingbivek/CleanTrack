64aafb019084976acf452c9101209d653664a757
Bivek Thapa <beingbivek@gmail.com>
2025-12-16 20:22:35 +0545

    formatted code

--- app/src/main/java/com/example/cleantrack/model/UserModel.kt
+++ app/src/main/java/com/example/cleantrack/model/UserModel.kt
@@ -1,16 +1,15 @@
 package com.example.cleantrack.model
 
 data class UserModel(
-    val email : String = "",
-    val fullname : String = "",
-    val number : String = "",
-    val role : String = "USER",
-    val userId : String = "",
-){
-    fun toMap() : Map<String, Any?>{
+    val email: String = "",
+    val fullname: String = "",
+    val number: String = "",
+    val role: String = "USER",
+    val userId: String = "",
+) {
+    fun toMap(): Map<String, Any?> {
         return mapOf(
-
-          "email" to email,
+            "email" to email,
             "fullname" to fullname,
             "number" to number,
             "role" to role,
