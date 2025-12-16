63a63910215be1e4b4419d739e92c78e39f84c84
Bivek Thapa <beingbivek@gmail.com>
2025-12-16 20:21:30 +0545

    Privacy Policy model created

--- app/src/main/java/com/example/cleantrack/model/PrivacyPolicyModel.kt
+++ app/src/main/java/com/example/cleantrack/model/PrivacyPolicyModel.kt
@@ -1,10 +1,15 @@
 package com.example.cleantrack.model
 
-import java.sql.Timestamp
-
 data class PrivacyPolicyModel(
-    val date : Timestamp,
-    val description : String,
-    val privacypolicyId : String,
-
-)
+    val privacypolicyId: String = "policy_1",
+    val description: String = "",
+    val date: Long = 0L
+) {
+    fun toMap(): Map<String, Any?> {
+        return mapOf(
+            "privacypolicyId" to privacypolicyId,
+            "description" to description,
+            "date" to date
+        )
+    }
+}
