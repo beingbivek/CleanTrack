7aa3e8b0e145f1dae284dd3b09b266edf6865d54
Bivek Thapa <beingbivek@gmail.com>
2025-12-20 22:02:29 +0545

    Vehicle repo

--- app/src/main/java/com/example/cleantrack/repository/VehicleRepo.kt
+++ app/src/main/java/com/example/cleantrack/repository/VehicleRepo.kt
@@ -4,9 +4,27 @@ import com.example.cleantrack.model.VehicleModel
 
 interface VehicleRepo {
 
-    fun addVehicle(model: VehicleModel, callback: (Boolean, String) -> Unit)
+    fun addVehicle(
+        model: VehicleModel,
+        callback: (Boolean, String) -> Unit
+    )
 
-    fun getAllVehicles(callback: (Boolean, String, List<VehicleModel>) -> Unit)
+    fun getAllVehicles(
+        callback: (Boolean, String, List<VehicleModel>) -> Unit
+    )
 
-    fun deactivateVehicle(vehicleId: String, callback: (Boolean, String) -> Unit)
+    fun getVehicleById(
+        vehicleId: String,
+        callback: (Boolean, String, VehicleModel?) -> Unit
+    )
+
+    fun updateVehicle(
+        model: VehicleModel,
+        callback: (Boolean, String) -> Unit
+    )
+
+    fun deleteVehicle(
+        vehicleId: String,
+        callback: (Boolean, String) -> Unit
+    )
 }
