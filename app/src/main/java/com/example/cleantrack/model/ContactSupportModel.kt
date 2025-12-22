ca4cbec3be34a011401ff7a08ea92eae3360d521
ishan <ishanbashyal11@gmail.com>
2025-12-22 22:31:16 +0545

    Model updated with map for changes

--- app/src/main/java/com/example/cleantrack/model/ContactSupportModel.kt
+++ app/src/main/java/com/example/cleantrack/model/ContactSupportModel.kt
@@ -1,6 +1,7 @@
 package com.example.cleantrack.model
 
 import android.os.Parcelable
+import com.google.firebase.database.Exclude
 import kotlinx.parcelize.Parcelize
 
 @Parcelize
@@ -16,4 +17,9 @@ data class ContactSupportModel(
     val userType: String = "Guest",
     val status: String = "OPEN",
     val timestamp: Long = System.currentTimeMillis()
-) : Parcelable
+) : Parcelable {
+
+    fun createReply(text: String): ContactSupportModel {
+        return this.copy(adminReply = text, status = "REPLIED")
+    }
+}
