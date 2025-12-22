560c9e9284340fd168e3a26c76e501b052609965
ishan <ishanbashyal11@gmail.com>
2025-12-22 09:03:49 +0545

    Removed mapping in contact support model

--- app/src/main/java/com/example/cleantrack/model/ContactSupportModel.kt
+++ app/src/main/java/com/example/cleantrack/model/ContactSupportModel.kt
@@ -1,14 +1,13 @@
 package com.example.cleantrack.model
 
 import android.os.Parcelable
-import com.google.firebase.database.Exclude
 import kotlinx.parcelize.Parcelize
 
 @Parcelize
 data class ContactSupportModel(
     val issueId: String = "",
     val userId: String = "",
-    val userType: String = "GUEST", // REGISTERED | GUEST
+    val userType: String = "GUEST",
     val fullname: String = "",
     val email: String = "",
     val category: String = "",
@@ -16,19 +15,4 @@ data class ContactSupportModel(
     val attachmentUrl: String = "",
     val timestamp: Long = System.currentTimeMillis()
 ) : Parcelable {
-
-    @Exclude
-    fun toMap(): Map<String, Any?> {
-        return mapOf(
-            "issueId" to issueId,
-            "userId" to userId,
-            "userType" to userType,
-            "fullname" to fullname,
-            "email" to email,
-            "category" to category,
-            "message" to message,
-            "attachmentUrl" to attachmentUrl,
-            "timestamp" to timestamp
-        )
-    }
 }
