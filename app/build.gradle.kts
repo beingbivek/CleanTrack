7bb47141350929bb2d0abfcae76f799e6cad481b
Aayush-cod <aayush0510bohara@gmail.com>
2025-12-23 18:13:34 +0545

    dependencies added in build gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -88,6 +88,7 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
 
     implementation("androidx.compose.runtime:runtime-livedata:<compose_version>")
+    implementation(libs.androidx.ui)
     androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
 
