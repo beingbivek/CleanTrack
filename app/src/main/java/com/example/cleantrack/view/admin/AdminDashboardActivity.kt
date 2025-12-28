9f7e9dc634d55574c88ca785ec45db0ccdf39909
Aayush-cod <aayush0510bohara@gmail.com>
2025-12-28 22:28:53 +0545

    Navigation added in admin dashboard for announcements

--- app/src/main/java/com/example/cleantrack/view/admin/AdminDashboardActivity.kt
+++ app/src/main/java/com/example/cleantrack/view/admin/AdminDashboardActivity.kt
@@ -27,6 +27,7 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.cleantrack.repository.UserRepoImpl
 import com.example.cleantrack.ui.theme.Red
+import com.example.cleantrack.view.admin.ui.theme.AdminAnnouncementListActivity
 import com.example.cleantrack.view.common.PrivacyPolicyActivity
 import com.example.cleantrack.view.auth.StartActivity
 import com.example.cleantrack.view.common.LogoutDialog
@@ -186,7 +187,7 @@ fun AdminDashboardScreen() {
                     icon = Icons.Default.Announcement
                 ) {
                     context.startActivity(
-                        Intent(context, AdminPointsRuleListActivity::class.java)
+                        Intent(context, AdminAnnouncementListActivity::class.java)
                     )
                 }
             }
