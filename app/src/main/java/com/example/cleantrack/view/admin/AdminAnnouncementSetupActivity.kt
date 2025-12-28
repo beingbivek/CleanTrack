d0af9352817d662b9fdcc791fe2e49e80c0517da
Aayush-cod <aayush0510bohara@gmail.com>
2025-12-28 22:52:53 +0545

    Changes in AdminAnnouncementSetupActivity.kt

--- app/src/main/java/com/example/cleantrack/view/admin/AdminAnnouncementSetupActivity.kt
+++ app/src/main/java/com/example/cleantrack/view/admin/AdminAnnouncementSetupActivity.kt
@@ -56,7 +56,7 @@ fun AdminAnnouncementSetupScreen(announcementId: String?) {
     // 1. Load data if in Edit mode
     LaunchedEffect(Unit) {
         if (announcementId != null) {
-            isLoading = true
+            isLoading = false
             announcementVM.getAllAnnouncements { _, _, _ ->
                 isLoading = false
             }
