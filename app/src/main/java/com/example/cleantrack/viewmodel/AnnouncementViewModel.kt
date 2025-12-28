e74079a390c9c099161a63916128c79939c0a1d7
Aayush-cod <aayush0510bohara@gmail.com>
2025-12-28 22:02:27 +0545

    AnnouncementViewModel Added

---
+++ app/src/main/java/com/example/cleantrack/viewmodel/AnnouncementViewModel.kt
@@ -0,0 +1,47 @@
+package com.example.cleantrack.viewmodel
+
+import android.util.Log
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.cleantrack.model.AnnouncementModel
+import com.example.cleantrack.model.UserModel
+import com.example.cleantrack.repository.AnnouncementRepo
+
+class AnnouncementViewModel(val repo : AnnouncementRepo) : ViewModel() {
+
+    fun postAnnouncement(model: AnnouncementModel, callback: (Boolean, String) -> Unit){
+
+        repo.postAnnouncement( model, callback)
+
+    }
+
+    private val _allAnnouncements = MutableLiveData<List<AnnouncementModel>?>()
+    val allUsers : MutableLiveData<List<AnnouncementModel>?>
+        get() = _allAnnouncements
+
+    fun getAllAnnouncements(callback: (Boolean, String, List<AnnouncementModel>) -> Unit){
+
+        repo.getAllAnnouncements{
+                success, message, data ->
+            Log.d("checkpoint",success.toString())
+            if (success){
+                _allAnnouncements.postValue(data)
+
+
+            }else {
+                // Log the error message to debug connection issues
+                println("Error fetching users: $message")
+                _allAnnouncements.postValue(emptyList()) // Post empty list on failure
+
+            }
+
+        }
+    }
+
+
+    fun deleteAnnouncement(id: String, callback: (Boolean, String) -> Unit){
+        repo.deleteAnnouncement(id, callback)
+
+    }
+
+}
