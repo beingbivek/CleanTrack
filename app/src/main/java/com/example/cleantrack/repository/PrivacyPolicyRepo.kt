2c8247d39e6758ee2db1bba992f9b81746963210
Mukesh-coder7 <universalbabu77@gmail.com>
2026-01-05 21:47:43 +0545

    Privacypolicy repo created

--- app/src/main/java/com/example/cleantrack/repository/PrivacyPolicyRepo.kt
+++ app/src/main/java/com/example/cleantrack/repository/PrivacyPolicyRepo.kt
@@ -1,10 +1,7 @@
 package com.example.cleantrack.repository
 
-import com.example.cleantrack.model.PrivacyPolicyModel
 
 interface PrivacyPolicyRepo {
-
-    fun getPrivacyPolicy(callback: (Boolean, String, PrivacyPolicyModel?) -> Unit)
-
-    fun savePrivacyPolicy(model: PrivacyPolicyModel, callback: (Boolean, String) -> Unit)
+    fun updatePrivacyPolicy(content: String, callback: (Boolean, String) -> Unit)
+    fun getPrivacyPolicy(callback: (Boolean, String, String?) -> Unit)
 }
