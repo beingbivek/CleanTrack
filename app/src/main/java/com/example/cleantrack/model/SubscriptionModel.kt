5dd18929c9720eaecbb4269c4e19d3ea49a1587d
Aayush-cod <aayush0510bohara@gmail.com>
2026-01-16 18:46:01 +0545

    SubscriptionModel.kt edited added parcealize

--- app/src/main/java/com/example/cleantrack/model/SubscriptionModel.kt
+++ app/src/main/java/com/example/cleantrack/model/SubscriptionModel.kt
@@ -1,9 +1,13 @@
 package com.example.cleantrack.model
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize // Add this
 data class SubscriptionModel(
     val isSubscribed: Boolean = false,
     val startDate: Long = 0,
     val expiryDate: Long = 0,
     val planType: String = "Monthly",
     val lastTransactionId: String = ""
-)
+) : Parcelable // Add this
