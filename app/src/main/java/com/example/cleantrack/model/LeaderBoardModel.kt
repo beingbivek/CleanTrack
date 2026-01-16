1e1f386e8b611d75ff4fd009e81178dc4fdfbbbf
Mukesh-coder7 <mukeshbabuacharya@gmail.com>
2026-01-16 12:38:23 +0545

    LeaderBoard Model created

---
+++ app/src/main/java/com/example/cleantrack/model/LeaderBoardModel.kt
@@ -0,0 +1,8 @@
+package com.example.cleantrack.model
+
+data class LeaderBoardUser(
+    val userId: String = "",
+    val fullname: String = "",
+    val points: Int = 0,
+    val profileImageUrl: String = ""
+)
