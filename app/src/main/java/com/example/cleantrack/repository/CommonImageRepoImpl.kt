d80cf056f04b07bf1dcceaa0efd77b31c3d0ce0d
Bivek Thapa <beingbivek@gmail.com>
2026-01-17 22:03:09 +0545

    Use Remote Config Cloudinary cloudname, api and secret keys

--- app/src/main/java/com/example/cleantrack/repository/CommonImageRepoImpl.kt
+++ app/src/main/java/com/example/cleantrack/repository/CommonImageRepoImpl.kt
@@ -8,18 +8,12 @@ import android.os.Looper
 import android.provider.OpenableColumns
 import com.cloudinary.Cloudinary
 import com.cloudinary.utils.ObjectUtils
+import com.google.android.gms.tasks.Tasks
+import com.google.firebase.remoteconfig.FirebaseRemoteConfig
 import java.io.InputStream
 import java.util.concurrent.Executors
 
 class CommonImageRepoImpl: CommonImageRepo {
-    private val cloudinary = Cloudinary(
-        mapOf(
-            "cloud_name" to "dljjon7od",
-            "api_key" to "833515826694124",
-            "api_secret" to "Qtl192CW306ZiaF927guCsdHBrA"
-        )
-    )
-
     override fun uploadImage(
         context: Context,
         imageUri: Uri,
@@ -28,6 +22,30 @@ class CommonImageRepoImpl: CommonImageRepo {
         val executor = Executors.newSingleThreadExecutor()
         executor.execute {
             try {
+                val remoteConfig = FirebaseRemoteConfig.getInstance()
+                Tasks.await(remoteConfig.fetchAndActivate())
+                val cloudName = remoteConfig.getString("cloudinary_cloud_name")
+                    .takeIf { it.isNotBlank() }
+                val apiKey = remoteConfig.getString("cloudinary_api_key")
+                    .takeIf { it.isNotBlank() }
+                val apiSecret = remoteConfig.getString("cloudinary_secret_key")
+                    .takeIf { it.isNotBlank() }
+
+                if (cloudName == null || apiKey == null || apiSecret == null) {
+                    Handler(Looper.getMainLooper()).post {
+                        callback(null)
+                    }
+                    return@execute
+                }
+
+                val cloudinary = Cloudinary(
+                    mapOf(
+                        "cloud_name" to cloudName,
+                        "api_key" to apiKey,
+                        "api_secret" to apiSecret
+                    )
+                )
+
                 val inputStream: InputStream? = context.contentResolver.openInputStream(imageUri)
                 var fileName = getFileNameFromUri(context, imageUri)
 
