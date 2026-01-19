f436d9fe899e2cffcffa2ac5f0648d28cbf7e04c
Mukesh-coder7 <mukeshbabuacharya@gmail.com>
2026-01-19 15:34:01 +0545

    Wrong Leaderboard model

--- app/src/main/java/com/example/cleantrack/model/LeaderBoardModel.kt
+++
@@ -1,8 +0,0 @@
-package com.example.cleantrack.model
-
-data class LeaderBoardUser(
-    val userId: String = "",
-    val fullname: String = "",
-    val points: Int = 0,
-    val profileImageUrl: String = ""
-)
