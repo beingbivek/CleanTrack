9833ba5640f52c2f12993fd6aabe70582036655c
Mukesh-coder7 <mukeshbabuacharya@gmail.com>
2026-01-19 15:35:00 +0545

    Model name change from leaderboadruser to leaderboardmodel

--- app/src/main/java/com/example/cleantrack/viewmodel/LeaderBoardViewModel.kt
+++ app/src/main/java/com/example/cleantrack/viewmodel/LeaderBoardViewModel.kt
@@ -2,13 +2,13 @@ package com.example.cleantrack.viewmodel
 
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
-import com.example.cleantrack.model.LeaderBoardUser
+import com.example.cleantrack.model.LeaderboardModel
 import com.example.cleantrack.repository.PointsRepo
 
 class LeaderboardViewModel(private val repository: PointsRepo) : ViewModel() {
 
     // Ensure this name matches what the Activity calls
-    val leaderboardState = mutableStateOf<List<LeaderBoardUser>>(emptyList())
+    val leaderboardState = mutableStateOf<List<LeaderboardModel>>(emptyList())
     val isLoading = mutableStateOf(true)
 
     init {
